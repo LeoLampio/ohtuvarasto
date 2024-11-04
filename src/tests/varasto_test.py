12b4a17f0e7135bca56a20a448094a291201c2bc
Leo Lampio <149969862+LeoLampio@users.noreply.github.com>
2024-11-04 20:47:51 +0200

    Test

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,37 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_tilavuus_nollataan(self):
+        v = Varasto(0, -1)
+        self.assertAlmostEqual(v.tilavuus, 0)
+
+    def test_saldo_nollataan(self):
+        v = Varasto(0, -1)
+        self.assertAlmostEqual(v.saldo, 0)
+
+    def test_ylijaama(self):
+        v = Varasto(1, 2)
+        self.assertAlmostEqual(v.saldo, 1)
+
+    def test_ei_negatiivista(self):
+        v = self.varasto.lisaa_varastoon(-1)
+        self.assertEqual(v, None)
+
+    def test_liikaa(self):
+        self.varasto.lisaa_varastoon(20)
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_ota_negatiivinen(self):
+        v = self.varasto.ota_varastosta(-1)
+        self.assertAlmostEqual(v, 0)
+
+    def test_ota_liikaa(self):
+        self.varasto.lisaa_varastoon(3)
+        v = self.varasto.ota_varastosta(4)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+        self.assertAlmostEqual(v, 3)
+
+    def test_str(self):
+        v = self.varasto.__str__()
+        self.assertEqual(v, "saldo = 0, vielä tilaa 10")
